7417db2353b33752d87e15aa094343c3c149907d
josef <91252200+Josef-Hlink@users.noreply.github.com>
2025-05-01 20:02:28 +0200

    visual multi

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -14,4 +14,17 @@ return {
       require('Comment').setup {}
     end,
   },
+  -- vim-visual-multi
+  {
+    'mg979/vim-visual-multi',
+    -- init = function()
+    --   vim.g.VM_default_mappings = 1
+    --   vim.g.VM_maps = {
+    --     ['Find Under'] = '<C-d>',
+    --     ['Find Subword Under'] = '<C-d>',
+    --     ['Add Cursor Down'] = '<C-Down',
+    --     ['Add Cursor Up'] = 'C-Up',
+    --   }
+    -- end,
+  },
 }
