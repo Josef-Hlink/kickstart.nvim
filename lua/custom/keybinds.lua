0abfdf7eaf46aca85d4b74b71e58906a48c3db49
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-08-31 16:06:26 +0200

    write with leader w

--- lua/custom/keybinds.lua
+++ lua/custom/keybinds.lua
@@ -7,3 +7,6 @@ vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
 -- Center window when moving with ctrl u/d
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+
+-- Write with leader w
+vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
